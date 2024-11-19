e2eec823eacde27b740f0e703d14ca3ba495de69
yunhwan <sky10240107@naver.com>
2024-11-19 22:42:41 +0900

    feat: layout 및 home 설정

---
+++ app/components/NavBr.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Link from "@/node_modules/next/link";
+import { usePathname, useRouter } from "@/node_modules/next/navigation";
+
+export default function NavBar() {
+  const pathname = usePathname();
+  return (
+    <nav>
+      <img src="/vercel.svg" />
+      <Link href="/">Home</Link>
+      <Link href="/food">Food</Link>
+      <style jsx>{`
+        nav {
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
+        }
+        .active {
+          color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
+        }
+      `}</style>
+    </nav>
+  );
+}
